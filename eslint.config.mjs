f73117313cc636ba18eae4dd770739ec81fb8501
vuwase <v.uwase@alustudent.com>
2025-06-30 14:09:29 +0200

    Delete all files

--- eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
-
-export default defineConfig([
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
